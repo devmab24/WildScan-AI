e211d741488587bdbbd039906ddb6fc6cb4df72e
Muhammad Ayuba Bello <talk2muhammadbello@gmail.com>
2026-04-29 18:58:15 +0100

    Fixed index mapping issue

--- app/app.py
+++ app/app.py
@@ -21,11 +21,17 @@ def get_model():
 
 model = get_model()
 
-# CLASS LABELS
-classes = [
-    "bird", "monkey_prosimian", "leopard", "hog",
-    "civet_genet", "antelope_duiker", "blank", "rodent"
-]
+# CLASS LABELS(As per the model's training)
+# idx_to_class = {
+#     0: "antelope_duiker",
+#     1: "bird",
+#     2: "blank",
+#     3: "civet_genet",
+#     4: "hog",
+#     5: "leopard",
+#     6: "monkey_prosimian",
+#     7: "rodent"
+# }
 
 # FILE UPLOAD
 uploaded_file = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
@@ -38,7 +44,8 @@ if uploaded_file:
         st.warning("Model not loaded.")
     else:
         tensor = preprocess(image)
-        result, confidence = predict(model, tensor, classes)
+        result, confidence = predict(model, tensor)
+        # result, confidence = predict(model, tensor, idx_to_class)
 
         st.success(f"Prediction: **{result}**")
         st.write(f"Confidence: {confidence:.2%}")
--- services/inference.py
+++ services/inference.py
@@ -2,6 +2,16 @@ import torch
 import torch.nn.functional as F
 from model.model import build_model
 
+import json
+import torch
+
+# Load class mapping
+with open("model/class_to_idx.json", "r") as f:
+    class_to_idx = json.load(f)
+
+# Convert to index → class
+idx_to_class = {v: k for k, v in class_to_idx.items()}
+
 def load_model():
     model = build_model()
     model.load_state_dict(
@@ -10,11 +20,36 @@ def load_model():
     model.eval()
     return model
 
-def predict(model, tensor, classes):
+def predict(model, tensor):
+    model.eval()
+
     with torch.no_grad():
         output = model(tensor)
-        probs = F.softmax(output, dim=1)
 
-        top_prob, top_class = torch.max(probs, 1)
+        probs = torch.softmax(output, dim=1)
+        confidence, pred = torch.max(probs, 1)
 
-    return classes[top_class.item()], top_prob.item()
+        label = idx_to_class[pred.item()]
+
+    return label, confidence.item()
+# #Prediction function
+# def predict(model, tensor, idx_to_class):
+#     model.eval()
+
+#     with torch.no_grad():
+#         output = model(tensor)
+
+#         probs = torch.softmax(output, dim=1)
+#         confidence, pred = torch.max(probs, 1)
+
+#         label = idx_to_class[pred.item()]
+
+#     return label, confidence.item()
+# def predict(model, tensor, classes):
+#     with torch.no_grad():
+#         output = model(tensor)
+#         probs = F.softmax(output, dim=1)
+
+#         top_prob, top_class = torch.max(probs, 1)
+
+#     return classes[top_class.item()], top_prob.item()
